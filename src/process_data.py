a688204b722074852049810ba8ebba2ecda7c9af
Victor de la Pompa <vpp020294@gmail.com>
2020-07-25 22:16:57 +0200

    Added super hero class and moved web scrapping to this new class

--- src/process_data.py
+++ src/process_data.py
@@ -1,14 +1,10 @@
-import re
 from tqdm import tqdm
 
+from superhero import SuperHero
 from utils import url2text, get_all_urls, get_all_urls_list
 
 
-class SuperHeroDataProcessor():
-    POWER_ABILITIES_SECTION = '## Powers and Abilities'
-    POWER_SECTION = '### Powers'
-    ABILITIES_SECTION = '### Abilities'
-    POWERS_PATTERN = '[*] [*][*].+:[*][*]'
+class SuperHeroDataExtractor():
     MARVEL_BASE_PAGE = 'https://marvel.fandom.com/'
     """ Class that extracts information from Marvel
         Fandom webpages.
@@ -26,12 +22,12 @@ class SuperHeroDataProcessor():
     def __init__(self, urls=None, all_characters=False):
         self.urls = urls
         if all_characters:
-            self.urls = SuperHeroDataProcessor._get_all_character_urls()
+            self.urls = SuperHeroDataExtractor._get_all_character_urls()
 
     @classmethod
     def _get_all_character_urls(cls):
 
-        marvel_character_page = SuperHeroDataProcessor.MARVEL_BASE_PAGE + 'wiki/Category:Characters'
+        marvel_character_page = SuperHeroDataExtractor.MARVEL_BASE_PAGE + 'wiki/Category:Characters'
 
         # The list of urls is filter by character first letter
         urls = get_all_urls(marvel_character_page, 'marvel.fandom.com', include_http=True)
@@ -47,7 +43,7 @@ class SuperHeroDataProcessor():
             character_urls = get_all_urls_list(url, web_page='wiki/',
                                                include_http=False,
                                                next_button_html='[ Next\n]')
-            character_urls = [SuperHeroDataProcessor.MARVEL_BASE_PAGE
+            character_urls = [SuperHeroDataExtractor.MARVEL_BASE_PAGE
                               + url.split('"')[0].replace('\\', '').strip()
                               for url in character_urls if '"' in url]
             all_urls = all_urls.union(character_urls)
@@ -59,63 +55,25 @@ class SuperHeroDataProcessor():
 
         Returns
         -------
-        texts_transformed : dict<string><string>
+        texts_transformed : dict<string><list<string>>
             Dictionary where keys are the url and the value
             is a list of strings with the information.
-
         """
         texts = url2text(self.urls, ignore_links=True)
 
         text_transformed = {}
 
-        for url, text in texts.items():
-            text = self.get_powers_text(text)
-            powers = self.extract_powers(text)
+        for url, text in tqdm(texts.items()):
+            sh = SuperHero(url, text)
+            powers = sh.powers
+
+            gallery_info = sh.get_gallery_information()
 
             # Get the name of the character
             name = url.split('/')[-1].replace('_', ' ')
 
-            text_transformed[url] = [name, powers]
+            text_transformed[url] = [name, powers, *gallery_info]
 
-        return text_transformed
+        columns = ['name', 'powers', *(SuperHero.GALLERY_PRETTY_FIELDS)]
 
-    def extract_powers(self, text):
-        """ Gets from the powers section a list of super powers.
-
-        Parameters
-        -------
-        text : string
-            Power section.
-
-        Returns
-        -------
-        list<string>
-            List of super powers.
-
-        """
-        texts = re.findall(self.POWERS_PATTERN, text)
-        # Remove undesired characters
-        texts = [t.lower().replace('*', '').replace('-', ' ').strip() for t in texts]
-
-        return texts
-
-    def get_powers_text(self, text):
-        """ Gets from the text only the powers section.
-
-        Parameters
-        -------
-        text : string
-            All the HTML transformed to string.
-
-        Returns
-        -------
-        string
-            Text with only the powers section.
-
-        """
-        idx = text.find(self.POWER_ABILITIES_SECTION)
-        text = text[idx:]
-        start = text.find(self.POWER_SECTION)
-        end = text.find(self.ABILITIES_SECTION)
-
-        return text[start:end]
+        return text_transformed, columns
---
+++ src/superhero.py
@@ -0,0 +1,103 @@
+import re
+
+
+class SuperHero():
+    POWERS_PATTERN = '[*] [*][*].+:[*][*]'
+    POWER_ABILITIES_SECTION = '## Powers and Abilities'
+    POWER_SECTION = '### Powers'
+    ABILITIES_SECTION = '### Abilities'
+    START_GALLERY_SECTION = 'Gallery'
+    END_GALLERY_SECTION = '## History'
+    GALLERY_RAW_FIELDS = ['Real Name', 'Current\nAlias\n', 'Aliases', 'Affiliation', 'Identity',
+                          'Citizenship', 'Occupation', 'Gender', 'Height', 'Weight',
+                          'Eyes', 'Hair', 'Origin', 'Universe', 'Creators']
+    GALLERY_PRETTY_FIELDS = ['Real Name', 'Current Alias', 'Aliases', 'Affiliation', 'Identity',
+                             'Citizenship', 'Occupation', 'Gender', 'Height', 'Weight',
+                             'Eyes', 'Hair', 'Origin', 'Universe', 'Creators']
+    """ Class that has information from one superhero
+
+    Attributes
+    -------
+    gallery_info : dict<string><string>
+        Dictionary where key is the field name and the value
+        is the value of that hero in that field.
+
+    Parameters
+    -------
+    url : string
+        URL with the information of the superhero.
+    html : string
+        HTML of the superhero.
+    """
+    def __init__(self, url, html):
+        self.url = url
+        self.html = html
+        self.gallery_info = None
+
+    @classmethod
+    def _get_text_between_two_strings(cls, text, start_string, end_string):
+        start = text.find(start_string)
+        text = text[start:]
+        end = text.find(end_string)
+
+        return text[:end]
+
+    @property
+    def powers(self):
+        """ List of super powers.
+        """
+        texts = re.findall(SuperHero.POWERS_PATTERN, self.html)
+        # Remove undesired characters
+        texts = [t.lower().replace('*', '').replace('-', ' ').strip() for t in texts]
+
+        return texts
+
+    def get_gallery_information(self):
+        """ Gets from the gallery section some information.
+
+        Returns
+        -------
+        list<string>
+            List with the information.
+        """
+        gallery_info = []
+        gallery_text = SuperHero._get_text_between_two_strings(self.html,
+                                                               SuperHero.START_GALLERY_SECTION,
+                                                               SuperHero.END_GALLERY_SECTION)
+        regex_number_brackets = re.compile(r'\[[0-9]+\]', re.IGNORECASE)
+        regex_multiple_spaces = re.compile(r' [ ]+', re.IGNORECASE)
+        for field_name in SuperHero.GALLERY_RAW_FIELDS:
+            # Every section seems to have a '###' at the end
+            value = SuperHero._get_text_between_two_strings(gallery_text,
+                                                            ') ' + field_name,
+                                                            '###')
+
+            # Delete the field_name from the value returned
+            value = value.replace(') ' + field_name, '')
+            # Delete special characters
+            value = value.replace('\n', '').strip()
+            # Delete numbers between [ ], and remove multiple spaces
+            value = regex_number_brackets.sub('', value)
+            value = regex_multiple_spaces.sub(' ', value)
+            gallery_info.append(value)
+
+        self.gallery_info = dict(zip(SuperHero.GALLERY_PRETTY_FIELDS, gallery_info))
+
+        return gallery_info
+
+    def get_powers_text(self):
+        """ Gets the powers section from the html text.
+
+        Returns
+        -------
+        string
+            Text with only the powers section.
+
+        """
+        idx = self.html.find(SuperHero.POWER_ABILITIES_SECTION)
+        text = self.html[idx:]
+        text = SuperHero._get_text_between_two_strings(text,
+                                                       SuperHero.POWER_SECTION,
+                                                       SuperHero.ABILITIES_SECTION)
+
+        return text
